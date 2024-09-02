1ff44516011598a669424cdbed6c9a14ea05ba67
SagarSalgarr <158045034+SagarSalgarr@users.noreply.github.com>
2024-09-02 17:21:25 +0530

    Updated the marqo.py and index.py to support marqo 2.11 ver (#16)
    * update marqo.py and index_document.py to support Marqo upgrade from version 1.5.1 to 2.11
    
    * Updated docker file and requirement-prod.txt
    
    * Added method level comment for cache_document and updated the requirements-prod.txt
    
    * Updated the code accept index_name from command line as argument
    
    * Made index_name generalised
    
    * Made changes to make generalise

--- vectorstores/marqo.py
+++ vectorstores/marqo.py
@@ -1,44 +1,39 @@
 import json
 import os
-from typing import (
-    Dict,
-    List,
-    Tuple
-)
-
+from typing import Dict, List, Tuple
 import marqo
 from langchain.docstore.document import Document
-from langchain_community.vectorstores import Marqo
-
+from langchain.vectorstores.marqo import Marqo
 from vectorstores.base import BaseVectorStore
 
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
 class MarqoVectorStore(BaseVectorStore):
     TENSOR_FIELDS: str = ["text"]
     client: marqo.Client
 
-    def __init__(self):
+    def __init__(self, index_name: str):
         self.client_url = os.environ["VECTOR_STORE_ENDPOINT"]
-        self.collection_name = os.environ["VECTOR_COLLECTION_NAME"]
         self.embedding_model = os.environ["EMBEDDING_MODEL"]
+        self.index_name = index_name  
         self.index_settings = {
-            "index_defaults": {
-                "treat_urls_and_pointers_as_images": False,
-                "model": self.embedding_model,
-                "normalize_embeddings": True,
-                "text_preprocessing": {
-                    "split_length": self.SPLIT_LENGTH,
-                    "split_overlap": self.SPLIT_OVERLAP,
-                    "split_method": "sentence"
-                }
+            "treatUrlsAndPointersAsImages": False,
+            "model": self.embedding_model,
+            "normalizeEmbeddings": True,
+            "textPreprocessing": {
+                "splitLength": self.SPLIT_LENGTH,
+                "splitOverlap": self.SPLIT_OVERLAP,
+                "splitMethod": "passage"
             }
         }
 
         if not self.client_url:
             raise ValueError("Missing environment variable VECTOR_STORE_ENDPOINT.")
 
-        if not self.collection_name:
-            raise ValueError("Missing environment variable VECTOR_COLLECTION_NAME.")
+        if not self.index_name:
+            raise ValueError("Missing collection name.")
 
         if not self.embedding_model:
             raise ValueError("Missing environment variable EMBEDDING_MODEL.")
@@ -49,17 +44,16 @@ class MarqoVectorStore(BaseVectorStore):
         return self.client
 
     def add_documents(self, documents=List[Document], fresh_collection: bool = False) -> List[str]:
-
         if fresh_collection:
             try:
-                self.client.index(self.collection_name).delete()
+                self.client.index(self.index_name).delete()
                 print("Existing Index successfully deleted.")
             except:
                 print("Index does not exist. Creating new index...")
 
             self.client.create_index(
-                self.collection_name, settings_dict=self.index_settings)
-            print(f"Index {self.collection_name} created.")
+                self.index_name, settings_dict=self.index_settings)
+            print(f"Index {self.index_name} created.")
 
         docs: List[Dict[str, str]] = []
         ids = []
@@ -71,7 +65,7 @@ class MarqoVectorStore(BaseVectorStore):
             docs.append(doc)
         chunks = list(self.chunk_list(docs, self.BATCH_SIZE))
         for chunk in chunks:
-            response = self.client.index(self.collection_name).add_documents(
+            response = self.client.index(self.index_name).add_documents(
                 documents=chunk, client_batch_size=self.BATCH_SIZE, tensor_fields=self.TENSOR_FIELDS)
             if response[0]["errors"]:
                 err_msg = (
@@ -83,7 +77,41 @@ class MarqoVectorStore(BaseVectorStore):
 
         return ids
 
-    def similarity_search_with_score(self, query: str, collection_name: str, k: int = 20) -> List[Tuple[Document, float]]:
-        docsearch = Marqo(self.client, index_name=collection_name, searchable_attributes=["text"])
-        documents = docsearch.similarity_search_with_score(query, k)
-        return documents
+    def similarity_search_with_score(self, query: str, index_name: str, k: int = 20) -> List[Tuple[Document, float]]:
+        try:
+            docsearch = Marqo(self.client, index_name=index_name)
+            documents = docsearch.similarity_search_with_score(query, k)
+            return documents
+        except Exception as e:
+            return []
+    
+    def cache_documents(self, documents: List[Dict[str, str]], index_name: str) -> str:
+        """
+        Caches documents into the specified Marqo collection. Creates the collection if it doesn't exist.
+
+        Args:
+            documents (List[Dict[str, str]]): List of documents with "text" and optional "metadata".
+            index_name (str): The target collection name.
+
+        Returns:
+            str: List of document IDs that were successfully cached.
+
+        Raises:
+            RuntimeError: If document upload fails.
+        """
+        try:
+            self.client.create_index(index_name, settings_dict=self.index_settings)
+        except Exception as e:
+            pass
+
+        response = self.client.index(index_name).add_documents(documents=documents,
+                                                         tensor_fields=self.TENSOR_FIELDS)
+        if response["errors"]:
+            err_msg = (
+                f"Error in uploading cache in index range {index_name}"
+                f"check Marqo logs."
+            )
+            raise RuntimeError(err_msg)
+        ids = []
+        ids += [item["_id"] for item in response["items"]]
+        return ids
